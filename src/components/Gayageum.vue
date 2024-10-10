8d8ee42f292508dbd3d81bb51982a21a428ec648
JerryPark <dev@teamflyers.kr>
2024-10-10 16:02:27 +0900

    Update Gayageum.vue

--- src/components/Gayageum.vue
+++ src/components/Gayageum.vue
@@ -593,7 +593,7 @@ function loadSound() {
 function playString(event, val, _selectedTuning, index) {
   return function (direction, mouseEvent) {
     console.log('>>>>>>>> playString', { direction, mouseEvent, event }, { _selectedTuning, selectedTechnic: selectedTechnic.value }, val, lastEventHandled.value);
-    if (lastEventHandled?.value?.eventType?.includes('move') && lastEventHandled?.value?.['구음'] === val['구음']) return;
+    // if (lastEventHandled?.value?.eventType?.includes('move') && lastEventHandled?.value?.['구음'] === val['구음']) return;
 
     (async () => {
       if (audioContext.state === 'suspended') {
