a21a8299579bc06ef490b57e04a0f82a6d273fe6
JerryPark <dev@teamflyers.kr>
2024-10-13 17:28:07 +0900

    Update Gayageum.vue

--- src/components/Gayageum.vue
+++ src/components/Gayageum.vue
@@ -618,7 +618,6 @@ function setSelectedTechnic(technic) {
 
 function openChrome() {
   if (/SamsungBrowser/i.test(navigator.userAgent)) {
-    alert("intent:" + window.location.host + "#Intent;scheme=https;package=com.android.chrome;end");
     alert("본 사이트는 크롬 브라우저 사용을 권장합니다.");
     window.location.href = "intent:" + window.location.host + "#Intent;scheme=https;package=com.android.chrome;end";
   }
