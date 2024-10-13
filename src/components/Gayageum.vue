fdaae2d44959ea2f281c2c21cddf23e2ded73c61
JerryPark <dev@teamflyers.kr>
2024-10-13 15:20:44 +0900

    Update Gayageum.vue

--- src/components/Gayageum.vue
+++ src/components/Gayageum.vue
@@ -552,14 +552,20 @@ function selectedNote(val, selectedNote, selectedTuning) {
 }
 
 async function getMedia(constrains) {
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia(initialContrains);
-    const video = document.createElement('video');
-    video.srcObject = stream;
-    video.muted = true;
-  } catch (error) {
-    console.log(error)
-  }
+  // if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+  //   const initialContrains = {
+  //     audio: true,
+  //     video: false
+  //   } // 특별한 요구사항 없이 오디오와 비디오 요청
+  //   try {
+  //     const stream = await navigator.mediaDevices.getUserMedia(initialContrains);
+  //     const video = document.createElement('video');
+  //     video.srcObject = stream;
+  //     video.muted = true;
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
 }
 
 function loadSound() {
